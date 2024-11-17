e016d3079a2224119b63c493bc88706b7ca6b702
Bruce Le <137387123+KhanhLeBigWow@users.noreply.github.com>
2024-11-17 14:42:21 -0500

    Additional code to integrate eventhub for logadd and logdelete feature

--- front-end/src/components/BaseComponent/BaseComponent.js
+++ front-end/src/components/BaseComponent/BaseComponent.js
@@ -1,3 +1,5 @@
+import { EventHub } from '../../eventhub/EventHub.js';
+
 export class BaseComponent {
   constructor() {
     this.cssLoaded = false;
@@ -22,6 +24,24 @@ export class BaseComponent {
     link.href = `./components/${fileName}/${fileName}.css`;
     document.head.appendChild(link);
     this.cssLoaded = true;
+  }
+
+  /**
+   * Subscribe to an event.
+   * @param {string} event - The event name.
+   * @param {function} listener - The callback function.
+   */
+  subscribe(event, listener) {
+    return this.eventHub.subscribe(event, listener);
+  }
+
+  /**
+ * Publish an event.
+ * @param {string} event - The event name.
+ * @param {any} data - The data to pass with the event.
+ */
+  publish(event, data) {
+    this.eventHub.publish(event, data);
   }
 
   dispatchCustomEvent(eventName, detail = {}) {
