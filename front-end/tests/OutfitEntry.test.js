4ec6625935fe54cb1807ff68ba4a95b5e239107c
Sophia Hubscher <33038334+sophia-hubscher@users.noreply.github.com>
2024-11-21 17:31:44 -0500

    Update tests

--- front-end/tests/OutfitEntry.test.js
+++ front-end/tests/OutfitEntry.test.js
@@ -1,37 +1,52 @@
 import { OutfitEntry } from "../src/models/OutfitEntry.js";
 
 describe('OutfitEntry', () => {
-    let outfit;
-    
-    beforeEach(() => {
-        outfit = new OutfitEntry('outfit1', 'user1', ['item1', 'item2'], 'Casual outfit');
-    });
+  let outfit;
 
-    test('should create an instance of OutfitEntry with given attributes', () => {
-        expect(outfit.outfit_id).toBe('outfit1');
-        expect(outfit.user_id).toBe('user1');
-        expect(outfit.wardrobe_item_ids).toEqual(['item1', 'item2']);
-        expect(outfit.note).toBe('Casual outfit');
-        expect(outfit.date).toBeInstanceOf(Date);
-        expect(outfit.created_at).toBeInstanceOf(Date);
-        expect(outfit.updated_at).toBeInstanceOf(Date);
-    });
+  beforeEach(() => {
+    outfit = new OutfitEntry(
+      'outfit1',
+      'user1',
+      ['item1', 'item2'],
+      'Casual outfit',
+      'casual',
+      ['Spring', 'Summer']
+    );
+  });
 
-    test('should update outfit details correctly', () => {
-        outfit.updateOutfit({ note: 'Updated note', wardrobe_item_ids: ['item3'] });
-        expect(outfit.note).toBe('Updated note');
-        expect(outfit.wardrobe_item_ids).toEqual(['item3']);
-        expect(outfit.updated_at).toBeInstanceOf(Date);
-    });
+  test('should create an instance of OutfitEntry with given attributes', () => {
+    expect(outfit.outfit_id).toBe('outfit1');
+    expect(outfit.user_id).toBe('user1');
+    expect(outfit.wardrobe_item_ids).toEqual(['item1', 'item2']);
+    expect(outfit.note).toBe('Casual outfit');
+    expect(outfit.occasion).toBe('casual');
+    expect(outfit.seasons).toEqual(['Spring', 'Summer']);
+    expect(outfit.created_at).toBeInstanceOf(Date);
+    expect(outfit.updated_at).toBeInstanceOf(Date);
+  });
 
-    test('should convert outfit to JSON correctly', () => {
-        const json = outfit.toJSON();
-        expect(json.outfit_id).toBe('outfit1');
-        expect(json.user_id).toBe('user1');
-        expect(json.wardrobe_item_ids).toEqual(['item1', 'item2']);
-        expect(json.note).toBe('Casual outfit');
-        expect(typeof json.date).toBe('string');
-        expect(typeof json.created_at).toBe('string');
-        expect(typeof json.updated_at).toBe('string');
-    });
+  test('should update outfit details correctly', () => {
+    const initialUpdatedAt = outfit.updated_at;
+    outfit.updateOutfit({ note: 'Updated note', wardrobe_item_ids: ['item3'] });
+
+    expect(outfit.note).toBe('Updated note');
+    expect(outfit.wardrobe_item_ids).toEqual(['item3']);
+    expect(outfit.updated_at).not.toBe(initialUpdatedAt);
+    expect(outfit.updated_at).toBeInstanceOf(Date);
+  });
+
+  test('should convert outfit to JSON correctly', () => {
+    const json = outfit.toJSON();
+
+    expect(json.outfit_id).toBe('outfit1');
+    expect(json.user_id).toBe('user1');
+    expect(json.wardrobe_item_ids).toEqual(['item1', 'item2']);
+    expect(json.note).toBe('Casual outfit');
+    expect(json.occasion).toBe('casual');
+    expect(json.seasons).toEqual(['Spring', 'Summer']);
+    expect(typeof json.created_at).toBe('string');
+    expect(typeof json.updated_at).toBe('string');
+    expect(new Date(json.created_at)).toBeInstanceOf(Date);
+    expect(new Date(json.updated_at)).toBeInstanceOf(Date);
+  });
 });
--- front-end/tests/WardrobeItem.test.js
+++ front-end/tests/WardrobeItem.test.js
@@ -1,68 +1,77 @@
 import { WardrobeItem } from "../src/models/WardrobeItem.js";
 
 describe('WardrobeItem', () => {
-    let wardrobeItem;
+  let wardrobeItem;
 
-    beforeEach(() => {
-        wardrobeItem = new WardrobeItem(
-            'item1',
-            'user1',
-            'image.jpg',
-            'Blue T-shirt',
-            20.00,
-            'M',
-            'tops',
-            'casual',
-            ['Spring', 'Summer'],
-            'BrandA'
-        );
+  beforeEach(() => {
+    wardrobeItem = new WardrobeItem({
+      item_id: 'item1',
+      user_id: 'user1',
+      image: 'image.jpg',
+      name: 'Blue T-shirt',
+      cost: 20.00,
+      size: 'M',
+      category: 'tops',
+      occasion: 'casual',
+      seasons: ['Spring', 'Summer'],
+      brand: 'BrandA',
     });
+  });
 
-    test('should create an instance of WardrobeItem with given attributes', () => {
-        expect(wardrobeItem.item_id).toBe('item1');
-        expect(wardrobeItem.user_id).toBe('user1');
-        expect(wardrobeItem.image).toBe('image.jpg');
-        expect(wardrobeItem.name).toBe('Blue T-shirt');
-        expect(wardrobeItem.cost).toBe(20.00);
-        expect(wardrobeItem.size).toBe('M');
-        expect(wardrobeItem.category).toBe('tops');
-        expect(wardrobeItem.occasion).toBe('casual');
-        expect(wardrobeItem.seasons).toEqual(['Spring', 'Summer']);
-        expect(wardrobeItem.brand).toBe('BrandA');
-        expect(wardrobeItem.is_favorite).toBe(false);
-        expect(wardrobeItem.times_worn).toBe(0);
-        expect(wardrobeItem.created_at).toBeInstanceOf(Date);
-        expect(wardrobeItem.updated_at).toBeInstanceOf(Date);
-    });
+  test('should create an instance of WardrobeItem with given attributes', () => {
+    expect(wardrobeItem.item_id).toBe('item1');
+    expect(wardrobeItem.user_id).toBe('user1');
+    expect(wardrobeItem.image).toBe('image.jpg');
+    expect(wardrobeItem.name).toBe('Blue T-shirt');
+    expect(wardrobeItem.cost).toBe(20.00);
+    expect(wardrobeItem.size).toBe('M');
+    expect(wardrobeItem.category).toBe('tops');
+    expect(wardrobeItem.occasion).toBe('casual');
+    expect(wardrobeItem.seasons).toEqual(['Spring', 'Summer']);
+    expect(wardrobeItem.brand).toBe('BrandA');
+    expect(wardrobeItem.is_favorite).toBe(false);
+    expect(wardrobeItem.times_worn).toBe(0);
+    expect(wardrobeItem.created_at).toBeInstanceOf(Date);
+    expect(wardrobeItem.updated_at).toBeInstanceOf(Date);
+  });
 
-    test('should update wardrobe item details correctly', () => {
-        wardrobeItem.updateItem({ name: 'Red T-shirt', cost: 25.00 });
-        expect(wardrobeItem.name).toBe('Red T-shirt');
-        expect(wardrobeItem.cost).toBe(25.00);
-        expect(wardrobeItem.updated_at).toBeInstanceOf(Date);
-    });
+  test('should update wardrobe item details correctly', () => {
+    const initialUpdatedAt = wardrobeItem.updated_at;
+    wardrobeItem.updateItem({ name: 'Red T-shirt', cost: 25.0 });
 
-    test('should increase times worn when wear is called', () => {
-        wardrobeItem.wear();
-        expect(wardrobeItem.times_worn).toBe(1);
-        expect(wardrobeItem.updated_at).toBeInstanceOf(Date);
-    });
+    expect(wardrobeItem.name).toBe('Red T-shirt');
+    expect(wardrobeItem.cost).toBe(25.00);
+    expect(wardrobeItem.updated_at).not.toBe(initialUpdatedAt);
+    expect(wardrobeItem.updated_at).toBeInstanceOf(Date);
+  });
 
-    test('should convert wardrobe item to JSON correctly', () => {
-        const json = wardrobeItem.toJSON();
-        expect(json.item_id).toBe('item1');
-        expect(json.user_id).toBe('user1');
-        expect(json.image).toBe('image.jpg');
-        expect(json.name).toBe('Blue T-shirt');
-        expect(json.cost).toBe(20.00);
-        expect(json.size).toBe('M');
-        expect(json.category).toBe('tops');
-        expect(json.occasion).toBe('casual');
-        expect(json.seasons).toEqual(['Spring', 'Summer']);
-        expect(json.brand).toBe('BrandA');
-        expect(json.is_favorite).toBe(false);
-        expect(json.times_worn).toBe(0);
-        expect(typeof json.created_at).toBe('string');
-        expect(typeof json.updated_at).toBe('string');
-    });
+  test('should increase times worn when wear is called', () => {
+    const initialUpdatedAt = wardrobeItem.updated_at;
+    wardrobeItem.wear();
+
+    expect(wardrobeItem.times_worn).toBe(1);
+    expect(wardrobeItem.updated_at).not.toBe(initialUpdatedAt);
+    expect(wardrobeItem.updated_at).toBeInstanceOf(Date);
+  });
+
+  test('should convert wardrobe item to JSON correctly', () => {
+    const json = wardrobeItem.toJSON();
+
+    expect(json.item_id).toBe('item1');
+    expect(json.user_id).toBe('user1');
+    expect(json.image).toBe('image.jpg');
+    expect(json.name).toBe('Blue T-shirt');
+    expect(json.cost).toBe(20.00);
+    expect(json.size).toBe('M');
+    expect(json.category).toBe('tops');
+    expect(json.occasion).toBe('casual');
+    expect(json.seasons).toEqual(['Spring', 'Summer']);
+    expect(json.brand).toBe('BrandA');
+    expect(json.is_favorite).toBe(false);
+    expect(json.times_worn).toBe(0);
+    expect(typeof json.created_at).toBe('string');
+    expect(typeof json.updated_at).toBe('string');
+    expect(new Date(json.created_at)).toBeInstanceOf(Date);
+    expect(new Date(json.updated_at)).toBeInstanceOf(Date);
+  });
 });
